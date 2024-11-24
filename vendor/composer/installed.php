f38d163d070420f378f310d38221570bd8d2da60
Jaronese96 <jaronese96@gmail.com>
2024-11-24 03:24:10 +0000

    Mundo guardado 2024-11-24

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ca84197d8fc64e5e231f6372fe7a2458ebb8acc0',
+        'reference' => 'a826c5a3c582d121c2070802f0b024bc63627af9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ca84197d8fc64e5e231f6372fe7a2458ebb8acc0',
+            'reference' => 'a826c5a3c582d121c2070802f0b024bc63627af9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
